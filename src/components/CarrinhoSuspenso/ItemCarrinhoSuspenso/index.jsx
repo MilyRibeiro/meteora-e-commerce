c9743aa9c8334518a6aee1f879b4b9c1da0dcbc3
Mylena Marques Ribeiro <ribeiro.mylena1@gmail.com>
2025-02-21 21:35:33 -0300

    Adicionando e removendo produtos

--- src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import Quantidade from "@/components/Quantidade";
 import Botao from "@/components/Botao";
 import ValorFormatado from "@/components/ValorFormatado";
+import { useCarrinhoContext } from "@/Hooks/useCarrinhoContext";
 
-const ItemCarrinhoSuspenso = ({
-  itemCarrinho,
-  removerProdutoCarrinho,
-  adicionarProduto,
-  removerProduto,
-}) => {
+const ItemCarrinhoSuspenso = ({ itemCarrinho }) => {
+  const { adicionarProduto, removerProduto, removerProdutoDoCarrinho } = useCarrinhoContext();
   return (
     <li>
       <>
@@ -30,7 +27,7 @@ const ItemCarrinhoSuspenso = ({
           <Botao
             variant="deleteItem"
             aria-label="Excluir"
-            onClick={() => removerProdutoCarrinho(itemCarrinho.id)}
+            onClick={() => removerProdutoDoCarrinho(itemCarrinho.id)}
           >
             delete_forever
           </Botao>
